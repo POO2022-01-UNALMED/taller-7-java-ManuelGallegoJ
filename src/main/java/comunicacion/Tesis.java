47572a4d6c1279c52e347648c3e9a8ac6c2a8b27
ManuelGallegoJ <92808245+ManuelGallegoJ@users.noreply.github.com>
2022-05-13 12:34:05 -0500

    Update Tesis.java

--- src/main/java/comunicacion/Tesis.java
+++ src/main/java/comunicacion/Tesis.java
@@ -60,5 +60,7 @@ public class Tesis extends Escrito {
 		return super.getPaginas() * palabrasPagina * 5;
 	}
 	
-
+	public String toString() {
+		return super.getOrigen() + "\n" + super.getTitulo() + "\n" + super.getAutor() + "\n" + super.getPaginas() + "\n" + this.idea + "\n" + this.argumentos.length + "\n" + this.conclusion + "\n" + this.referencias;
+	}
 }
